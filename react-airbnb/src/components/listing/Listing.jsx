9748508bb259523d18bcdb4b12874bc0bd5007e7
Huishan <hui_shan91@hotmail.com>
2022-09-04 22:26:18 +0800

    feat: added useEffect to call backend

--- react-airbnb/src/components/listing/Listing.jsx
+++ react-airbnb/src/components/listing/Listing.jsx
@@ -3,9 +3,24 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import React from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Listing = () => {
     const params = useParams();
+
+    // calling the backend API
+    const fetchApi = async () => {
+        // MAKE API CALL TO BACKEND HERE
+        const data = await axios.get("");
+        return data;
+    };
+
+    // Backend API is called upon component mounting
+    useEffect(() => {
+        (async () => {
+            const data = await fetchApi();
+        })();
+    }, []);
 
     return <div>Listing</div>;
 };
