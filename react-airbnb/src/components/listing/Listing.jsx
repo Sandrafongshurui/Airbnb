5cb091b67017149760ad24a2864f43f70ecf412b
Huishan <hui_shan91@hotmail.com>
2022-09-04 21:19:01 +0800

    feat: added axios package

--- react-airbnb/src/components/listing/Listing.jsx
+++ react-airbnb/src/components/listing/Listing.jsx
@@ -1,6 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Listing = () => {
+    const params = useParams();
+
     return <div>Listing</div>;
 };
 
