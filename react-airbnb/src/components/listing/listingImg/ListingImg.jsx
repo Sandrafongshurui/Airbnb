fd1b68c4f6f75ecfee2afaa9999a2a4d123ae7a0
Huishan <hui_shan91@hotmail.com>
2022-09-05 20:56:52 +0800

    feat: added image for sample

--- react-airbnb/src/components/listing/listingImg/ListingImg.jsx
+++ react-airbnb/src/components/listing/listingImg/ListingImg.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./ListingImg.css";
 
 const ListingImg = () => {
-    return <div>ListingImg</div>;
+    return (
+        <div className="listingImg">
+            ListingImg
+            <img
+                src="http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg"
+                layout="fill"
+                alt="Listing"
+                objectFit="cover"
+            />
+        </div>
+    );
 };
 
 export default ListingImg;
