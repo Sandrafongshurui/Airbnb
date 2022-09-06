97d773d077e2e59edfaa958f3080ff6b5230141a
Huishan <hui_shan91@hotmail.com>
2022-09-06 08:17:42 +0800

    feat: added basic styling to hosting info and listing map

--- react-airbnb/src/components/listing/listingHostInfo/ListingHostInfo.jsx
+++ react-airbnb/src/components/listing/listingHostInfo/ListingHostInfo.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./ListingHostInfo.css";
 
 const ListingHostInfo = () => {
-    return <div>ListingHostInfo</div>;
+    return (
+        <div className="ListingHostInfo">
+            <h3>ListingHostInfo</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                deleniti exercitationem voluptatem ratione iure placeat error
+                provident aspernatur dolore perferendis tempora ipsa, dolor
+                veniam perspiciatis earum quia commodi fugiat amet?
+            </p>
+        </div>
+    );
 };
 
 export default ListingHostInfo;
