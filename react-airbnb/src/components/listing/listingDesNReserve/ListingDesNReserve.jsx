df908902c82302a880bd2fd7e7f8388fca1c5c35
Huishan <hui_shan91@hotmail.com>
2022-09-06 14:47:24 +0800

    feat: adjusted css and styling

--- react-airbnb/src/components/listing/listingDesNReserve/ListingDesNReserve.jsx
+++ react-airbnb/src/components/listing/listingDesNReserve/ListingDesNReserve.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ListingDesNReserve.css";
+import { DateRangePicker } from "react-date-range";
 
-const ListingDesNReserve = () => {
-    return <div>ListingDesNReserve</div>;
+const ListingDesNReserve = (props) => {
+    // pass in props for description and price / night
+
+    return (
+        <div className="ListingDesNReserve">
+            <h3>ListingDesNReserve</h3>
+
+            <div className="sticky grid grid-cols-2">
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="col">Description</div>
+                        <div className="col">Price / Night</div>
+
+                        <button>Reserve</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ListingDesNReserve;
--- react-airbnb/src/components/listing/listingImg/ListingImg.jsx
+++ react-airbnb/src/components/listing/listingImg/ListingImg.jsx
@@ -4,7 +4,7 @@ import "./ListingImg.css";
 const ListingImg = () => {
     return (
         <div className="listingImg">
-            <p>ListingImg</p>
+            <h3>ListingImg</h3>
             <img
                 src="http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg"
                 layout="fill"
