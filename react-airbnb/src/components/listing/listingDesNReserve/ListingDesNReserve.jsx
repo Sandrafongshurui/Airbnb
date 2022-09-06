ce3fb342ede08a8123d81acc7a3bda6e54bab6a6
Huishan <hui_shan91@hotmail.com>
2022-09-06 18:49:04 +0800

    feat: updated no of guests feature

--- react-airbnb/src/components/listing/listingDesNReserve/ListingDesNReserve.jsx
+++ react-airbnb/src/components/listing/listingDesNReserve/ListingDesNReserve.jsx
@@ -3,20 +3,31 @@ import "./ListingDesNReserve.css";
 import { DateRangePicker } from "react-date-range";
 
 const ListingDesNReserve = (props) => {
+    const [noOfGuests, setNoOfGuests] = useState(1);
     // pass in props for description and price / night
+
+    const handleGuests = (e) => {
+        setNoOfGuests(e.target.value);
+    };
 
     return (
         <div className="ListingDesNReserve">
             <h3>ListingDesNReserve</h3>
 
-            <div className="sticky grid grid-cols-2">
-                <div className="container text-center">
-                    <div className="row">
-                        <div className="col">Description</div>
-                        <div className="col">
-                            Price / Night
-                            <button>Reserve</button>
-                        </div>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">Description</div>
+                    <div className="col">
+                        Price / Night
+                        <button>Reserve</button>
+                        <br />
+                        No of Guests:
+                        <input
+                            type="number"
+                            value={noOfGuests}
+                            onChange={handleGuests}
+                            min={1}
+                        ></input>
                     </div>
                 </div>
             </div>
