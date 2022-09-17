2d36579ce62ad378cb470ed74ba0515b19b77511
Sandrafongshurui <63345014+Sandrafongshurui@users.noreply.github.com>
2022-09-17 17:10:44 +0800

    Delete react-airbnb directory

--- react-airbnb/src/components/editTrip/EditTrip.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EditTrip = () => {
-    return <div>Edit</div>;
-};
-
-export default EditTrip;
--- react-airbnb/src/pages/createListings/utils.js
+++
@@ -1,9 +0,0 @@
-export const loadImageFromFile = (file) => {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            resolve(e.target.result);
-        }
-    reader.readAsDataURL(file);
-    })
-}
