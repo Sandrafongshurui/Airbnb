00e37db8cfd14b2bbe046fcac82faafc33e093de
sandrafong <sandrafong90@hotmail.com>
2022-09-17 21:26:44 +0800

    feat: npm build

--- src/components/list-listings/List-listings.jsx
+++ src/components/list-listings/List-listings.jsx
@@ -12,7 +12,7 @@ function ListListings({ isHost }) {
 
     useEffect(() => {
         const fetchApi = async () => {
-            const res = await fetch("http://localhost:8000/api/v1", {
+            const res = await fetch("https://ourairbnb.herokuapp.com//api/v1", {
                 headers: headerOptions,
             });
             const data = await res.json();
